5b02ac32da1f9ad09c284818f43dc03a9c1a49e0
AyushInnoage30 <ayush.singh@innoage.in>
2025-02-20 12:10:51 +0530

    Add project files.

---
+++ ConnectionMongoBd/DBConnection/DbContext.cs
@@ -0,0 +1,32 @@
+﻿using ConnectionMongoBd.Models.Entity;
+using MongoDB.Driver;
+
+namespace MongodbServices
+{
+    public class MOngoDBService
+    {
+
+        // _userContained hole data from MongoDB
+        private readonly IMongoCollection<User> _userCollection;
+
+        public MOngoDBService(IConfiguration config)
+        {
+            var client = new MongoClient(config["ConnectingStringDatabase:ConnectionString"]);
+            var database = client.GetDatabase(config["ConnectingStringDatabase:DatabaseName"]);
+            _userCollection = database.GetCollection<User>(config["ConnectingStringDatabase:UserCollection"]);
+        }
+
+
+        //For GetMethod
+        public async Task <List<User>> GetUserAsync()
+        {
+            return await _userCollection.Find(user => true).ToListAsync();
+        }
+
+        public async Task CreatedUserAsync(User user)
+        {
+            await _userCollection.InsertOneAsync(user);
+        }
+
+    }
+}
---
+++ ConnectionMongoBd/Models/Entity/User.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ConnectionMongoBd.Models.Entity
+{
+    public class User
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
